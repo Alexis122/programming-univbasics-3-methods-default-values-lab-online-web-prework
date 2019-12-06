cb507fb85547b387e9d4115e777bf408c1510c2f
Alexis Priore <alexispriore@gmail.com>
2019-12-06 19:28:18 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,14 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+
+protein = "Tofu"
+veg1 = "Broccoli"
+veg2 = "Macaroni"
+ 
+# With +
+protein + " " + veg1 + " " + veg2
+
+puts "What a nutritious meal!"
+expect (meal_choice("Broccoli", "Macaroni"))
+puts "A plate of #{protein} with #{veg1} and #{veg2}." 
